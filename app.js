79389b48d5599323291e06cba8777b4f9f83c4b0
LauritsSchou <laurits.schou@gmail.com>
2023-01-31 11:33:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("app.js is running 🎉");
 function sayHello() {
   document.querySelector("h3").textContent =
-    "Hej" + document.querySelector("input").value;
+    "Hej " + document.querySelector("input").value;
 }
 document.querySelector("button").addEventListener("click", sayHello);
